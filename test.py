fe89e65a78c8fcdd001c8aba7f4b867fa3abb8d0
ghostgod1 <73012930+ghostgod1@users.noreply.github.com>
2023-06-22 17:36:15 +0530

    done

---
+++ test.py
@@ -0,0 +1,61 @@
+from cvzone.HandTrackingModule import HandDetector
+import cv2
+import cvzone
+import numpy as np
+
+cap = cv2.VideoCapture(0)
+cap.set(3,1280)
+cap.set(4,720)
+
+detector = HandDetector(detectionCon=0.5, maxHands=1)
+
+def drawAll(img, button):
+     imgNew = np.zeros_like(img, np.uint8)
+     x, y = button.pos
+     cvzone.cornerRect(imgNew, (button.pos[0], button.pos[1], button.size[0], button.size[1]), 20, rt=0)
+     cv2.rectangle(imgNew, button.pos, (x + button.size[0], y + button.size[1]),(10, 10, 10), cv2.FILLED)
+     cv2.putText(imgNew, button.text, (x + 40, y + 60), cv2.FONT_HERSHEY_PLAIN, 3, (255, 255, 255), 4)
+
+     out = img.copy()
+     alpha = 0.5
+     mask = imgNew.astype(bool)
+     out[mask] = cv2.addWeighted(img, alpha, imgNew, 1 - alpha, 0)[mask]
+     return out
+
+class Button():
+    def __init__(self, pos, text, size=[250, 250]):
+        self.pos = pos
+        self.size = size
+        self.text = text
+    
+myButton3 = Button([500, 250], "center")
+
+while True:
+    success, img = cap.read()
+    hands, img = detector.findHands(img)
+    img = drawAll(img, myButton3)
+
+    if hands:
+        hand = hands[0]
+        lmList = hand["lmList"]
+        bbox = hand["bbox"]
+        centerPoint = hand['center']
+        handType = hand["type"]
+
+        #print(centerPoint)
+
+        if centerPoint[1]<300 and centerPoint[0]>300 and centerPoint[0]<700:
+            print("up")
+        if centerPoint[1]>400 and centerPoint[0]>300 and centerPoint[0]<700:
+            print("down")
+        if centerPoint[0]<300 and centerPoint[1]>300 and centerPoint[1]<700:
+            print("right")
+        if centerPoint[0]>700 and centerPoint[1]>300 and centerPoint[1]<700:
+            print("left")
+    
+
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
+    
+cap.release("q")
+cv2.destroyAllWindows()
