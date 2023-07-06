f9738e93567dd23de9394e99e03f209bcaea270e
ghostgod1 <73012930+ghostgod1@users.noreply.github.com>
2023-07-06 13:04:36 +0530

    angle

--- cross_new.py
+++ cross_new.py
@@ -1,10 +1,24 @@
 from dependencies.handtraking import HandDetector
 from dependencies.posemodule import posedetector
 import cv2
+import math
 
 cap = cv2.VideoCapture(0)
 cap.set(3,1280)
 cap.set(4,720)
+
+def Angle(p1, p2, p3):
+        
+    x1, y1 = lmListPose[p1][1:3]
+    x2, y2 = lmListPose[p2][1:3]
+    x3, y3 = p3
+
+    angle = math.degrees(math.atan2(y3 - y2, x3 - x2) -
+                         math.atan2(y1 - y2, x1 - x2))
+    if angle < 0:
+        angle += 360
+
+    return angle
 
 handDetect = HandDetector(detectionCon=0.5, maxHands=1)
 poseDetect = posedetector()
@@ -28,14 +42,14 @@ while True:
         color = (255, 0, 0)
         thickness = 2
 
-        a = [rightShoulder[0]+1000, rightShoulder[1] - 1000]
+        a = [rightShoulder[0]+1000, rightShoulder[1]-1000]
         b = [rightShoulder[0]-1000, rightShoulder[1]+1000]
 
         img = cv2.rectangle(img, start_point, end_point,color,thickness)
         img = cv2.line(img, start_point, end_point, color, thickness)
         img = cv2.line(img, a, b, color, thickness)
 
-        #print(rightShoulder)
+        print(Angle(12,11,start_point))
 
     #Hand Detection
     hands, img = handDetect.findHands(img)
@@ -63,6 +77,8 @@ while True:
         if landmark[1] > rightShoulder[1]:
             if landmark[1] > rightShoulder[1] and landmark[0] < (landmark[1]+rightShoulder[0]-rightShoulder[1]) and landmark[0] > (-landmark[1]+rightShoulder[0]+rightShoulder[1]):
                 print("Down side")
+
+    
 
     cv2.imshow("Image", img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
